af5dcfb1bd05e32311997ff28887be14e5e869a9
0seo8 <0seo23@gmail.com>
2022-06-24 23:47:35 +0900

    feat: store-product.js 생성 및 변수 설정

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from 'vuex'
 import admin from './admin'
+import product from './product'
 
 export default createStore({
   modules: {
-    admin
+    admin,
+    product
   }
 })
---
+++ src/store/product.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+const { VITE_API_KEY, VITE_USERNAME } = import.meta.env
+
+const END_POINT = 'https://asia-northeast3-heropy-api.cloudfunctions.net/api/products'
+
+const headers = {
+  'content-type': 'application/json',
+  'apikey': VITE_API_KEY,
+  'username': VITE_USERNAME
+}
+
+export default {
+  namespaced: true,
+
+}
