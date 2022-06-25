ebeeebb0cba79b92d61f38444ebe5914614740e2
0seo8 <0seo23@gmail.com>
2022-06-25 20:26:47 +0900

    fix: Sass 구성파일 재설정

---
+++ src/components/layouts/Logo.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <RouterLink
+      to="/"
+      class="logo">
+      <img
+        src="~/assets/logo.png"
+        alt="로고" />
+      <span>Eletronic</span>Market
+    </RouterLink>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+.logo {
+  font-family: "Oswald", sans-serif;
+  font-size: 20px;
+  color: $black;
+  span {
+    color: $primary;
+  }
+}
+</style>
