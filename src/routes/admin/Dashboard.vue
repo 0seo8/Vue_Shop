114eb33ffdbbb186122098c58ff4a2e7f45bdf9d
chan <cham9994@gmail.com>
2022-06-25 21:15:47 +0900

    config: 대시보드 차트 그리기

--- src/routes/admin/Dashboard.vue
+++ src/routes/admin/Dashboard.vue
@@ -61,6 +61,7 @@
             <article class="card-body">
               <div class="chartjs">
                 <h5>제품별 판매량(막대 그래프)</h5>
+                <canvas id="myChart"></canvas>
               </div>
             </article>
           </div>
@@ -70,6 +71,7 @@
             <article class="card-body">
               <div class="chartjs">
                 <h5>매출액 대비 제품 비중</h5>
+                <canvas id="donutChart"></canvas>
               </div>
             </article>
           </div>
@@ -84,8 +86,86 @@
   </section>
 </template>
 <script>
+import Chart from 'chart.js/auto'
 export default {
-  
+  mounted() {
+    console.log('Component mounted')
+
+    const productSales = document.querySelector('#myChart')
+    const saleOfProudcts = document.querySelector('#donutChart')
+    
+    const myChart = new Chart(productSales, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+})
+const doughnutChart = new Chart(saleOfProudcts, {
+    type: 'doughnut',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+})
+
+myChart
+doughnutChart
+  }
 }
 </script>
 <style lang="scss" scoped>
