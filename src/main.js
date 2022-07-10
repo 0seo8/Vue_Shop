f8df16ba58c6b32b0e82acdd90de1a0111e9f873
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-07-10 19:09:02 +0900

    UI폴더 제거

--- src/main.js
+++ src/main.js
@@ -2,8 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes'
 import store from './store/index.js'
-import BaseCard from './components/UI/BaseCard.vue'
 import VueSweetalert2 from 'vue-sweetalert2'
 import 'sweetalert2/dist/sweetalert2.min.css'
 
-createApp(App).use(router).use(store).use(VueSweetalert2).component('BaseCard', BaseCard).mount('#app')
+createApp(App).use(router).use(store).use(VueSweetalert2).mount('#app')
