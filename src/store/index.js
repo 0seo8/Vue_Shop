83bd6b264b4e49498887bf0d47e0b7cf18dcdfab
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-07-12 11:01:55 +0900

    user.js연결 해제

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,6 @@
 import { createStore } from 'vuex'
 import admin from './admin'
 import product from './product'
-import user from './user'
 import account from './account'
 import auth from './auth'
 
@@ -9,7 +8,6 @@ export default createStore({
   modules: {
     admin,
     product,
-    user,
     account,
     auth
   }
