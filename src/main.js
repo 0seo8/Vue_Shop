1a9c5f2050852fa0a85b088a91fdf2966ba4f2a4
0seo8 <96939562+0seo8@users.noreply.github.com>
2022-07-12 15:13:57 +0900

    fix: 부트스트랩 min으로 변경

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes'
@@ -6,3 +7,5 @@ import VueSweetalert2 from 'vue-sweetalert2'
 import 'sweetalert2/dist/sweetalert2.min.css'
 
 createApp(App).use(router).use(store).use(VueSweetalert2).mount('#app')
+
+import 'bootstrap/dist/js/bootstrap.js'
